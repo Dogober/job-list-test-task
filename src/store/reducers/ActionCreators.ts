5c6f72967cb508ff47c54e53386b66d8a88dfb1a
Nick <101208971+Dogober@users.noreply.github.com>
2022-11-10 22:15:51 +0200

    fix redux-types and displayed response-data

---
+++ src/store/reducers/ActionCreators.ts
@@ -0,0 +1,21 @@
+import { AppDispatch } from "../store";
+import axios, { AxiosError } from 'axios'
+import {jobListSlice} from "./jobListSlice";
+import { Job } from "../../models/Job";
+
+const bearerToken = 'wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu'
+
+export const fetchJobList = () => async (dispatch: AppDispatch) => {
+    try {
+        dispatch(jobListSlice.actions.jobListFetching())
+        const response = await axios.get<Job[]>('https://api.json-generator.com/templates/ZM1r0eic3XEy/data', {
+            params: {
+                access_token: bearerToken,
+            }
+        })
+        dispatch(jobListSlice.actions.jobListFetchingSuccess(response.data))
+    } catch (error) {
+        const er = error as AxiosError
+        dispatch(jobListSlice.actions.jobListFetchingError(er.message))
+    }
+}
