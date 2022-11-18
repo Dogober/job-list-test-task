2c636910f445676d113039b12a7d9a5748ab5fcd
Nick <101208971+Dogober@users.noreply.github.com>
2022-11-18 20:12:03 +0200

    delete calcPages

--- src/utilities/calcPages.ts
+++
@@ -1,6 +0,0 @@
-import { Job } from "../models/Job";
-
-export const calcPages = (limit: number, jobs: Job[]): string[] => {
-    const numberOfPages = Math.ceil(jobs.length/limit)
-    return Array(numberOfPages).fill('')
-}
