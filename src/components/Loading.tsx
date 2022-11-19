d13e69ef484ba94e863af816dc1367c4fea8f6f5
Nick <101208971+Dogober@users.noreply.github.com>
2022-11-19 20:47:33 +0200

    change circle`s animate borderWidth

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 const Loading: FC = () => {
     return (
         <div className="mt-[5%] text-[#3A4562] font-bold text-3xl flex gap-2 items-center">
-            <div className="w-7 h-7 rounded-full border-2 border-[#3A4562] border-dashed animate-spin"></div>
+            <div className="w-7 h-7 rounded-full border-4 border-[#3A4562] border-dashed animate-spin"></div>
             <p>Loading</p>
         </div>
     );
