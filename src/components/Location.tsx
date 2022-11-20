c34dbdfccbd8d45d11c7c8100816ac6a81296b17
Nick <101208971+Dogober@users.noreply.github.com>
2022-11-20 15:57:14 +0200

    transport Circle.png to ../assets

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -10,7 +10,7 @@ const Location: FC = () => {
     return (
         <div className="flex items-center flex-col h-[436px]">
             <div
-                style={{backgroundImage: 'url(/Circle.svg)'}}
+                style={{backgroundImage: `url(${require('../assets/Circle.png')}`}}
                 className=" bg-no-repeat bg-[#2A3047] text-[#E7EAF0] max-[640px]:max-w-[372px] max-w-[402px] w-full max-h-[218px] h-full rounded-t-lg pt-8 px-16 flex flex-col gap-2">
                 <div className="font-bold text-xl mb-2">
                     <p>
