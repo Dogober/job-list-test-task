f995d9e5c334bf3af3ec660737e2798ed16f5899
Nick <101208971+Dogober@users.noreply.github.com>
2022-11-20 19:30:51 +0200

    BrowserRouter => HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css'
 import { store } from './store/store';
@@ -10,8 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>      
+    </HashRouter>      
   </Provider>
 );
