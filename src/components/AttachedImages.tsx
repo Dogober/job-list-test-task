b955aa41b51aaa6291e8ba8ee592da0ac24154e8
Nick <101208971+Dogober@users.noreply.github.com>
2022-11-20 19:09:54 +0200

    adaptived Attached fotos for phonemode

--- src/components/AttachedImages.tsx
+++ src/components/AttachedImages.tsx
@@ -10,10 +10,10 @@ const AttachedImages: FC = () => {
             <div className="border-b pb-2 font-bold text-3xl mt-20 mb-6">
                 Attached images
             </div>
-            <div className="flex gap-3 max-[640px]:overflow-hidden">
+            <div className="flex gap-3 max-[640px]:overflow-x-auto scrollbar scrollbar-thumb-[#3A4562]/[.4] scroll-py-3">
                 {currentJob?.pictures.map((picture, i) =>
                 <img
-                    className="rounded-lg"
+                    className="rounded-lg mb-6 max-[640px]:mb-2"
                     key={i}
                     src={picture}/>
                 )}
