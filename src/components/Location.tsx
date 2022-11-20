1086e041bfea28986cd219c11fc60e49e70fb33b
Nick <101208971+Dogober@users.noreply.github.com>
2022-11-20 20:47:19 +0200

    adaptived Location style for phonemode

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -12,22 +12,24 @@ const Location: FC = () => {
             <div
                 style={{backgroundImage: `url(${require('../assets/Circle.png')}`}}
                 className=" bg-no-repeat bg-[#2A3047] text-[#E7EAF0] max-[640px]:max-w-[372px] max-w-[402px] w-full max-h-[218px] h-full rounded-t-lg pt-8 px-16 flex flex-col gap-2">
-                <div className="font-bold text-xl mb-2">
+                <div className="font-bold max-[640px]:text-base text-xl mb-2">
                     <p>
                         Department name.
                     </p>
-                    <p>
+                    <p className="font-bold max-[640px]:text-base text-xl">
                         {detailsDisplayJob?.name}.
                     </p>
                 </div>
                 <div className="flex gap-2">
                     <GpsIcon/>
-                    <p className="text-lg font-normal text-[#E8EBF3]">{detailsDisplayJob?.address}</p>
+                    <p className="max-[640px]:text-base text-lg text-[#E8EBF3]">{detailsDisplayJob?.address}</p>
                 </div>
-                <p>
-                    {detailsDisplayJob?.phone}, {detailsDisplayJob?.email}
+                <p className="max-[640px]:text-base text-lg max-[640px]:leading-3 leading-4">
+                    {detailsDisplayJob?.phone},
                 </p>
-                
+                <p className="max-[640px]:text-base text-lg max-[640px]:leading-3 leading-4">
+                    {detailsDisplayJob?.email}
+                </p>
             </div>
             <img
                 className="rounded-b-lg max-[640px]:hidden max-w-[402px] w-full"
